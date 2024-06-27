f1ef1edb1239b02b428ed3bc949596794a18cd02
montaser77789 <montasergohar@gmail.com>
2024-06-27 13:42:08 +0300

    Update swiger

--- js/index.js
+++ js/index.js
@@ -89,24 +89,6 @@ function removeShadow(inputBoxId) {
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper-container', {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
 
-      // If you need pagination
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  });
 
 
